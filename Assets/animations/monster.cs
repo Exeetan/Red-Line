67b0733cdfed5d5c1a8e59c613f8ea8dad939b0e
TheP2-extended <theP14159@gmail.com>
2026-09-24 19:41:34 +0200

    1.0.1
    asdasdasd

--- Assets/animations/monster.cs
+++ Assets/animations/monster.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class monster : MonoBehaviour
 {
     float t = 0;
+    
     [SerializeField] private GameObject[] attacks;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
